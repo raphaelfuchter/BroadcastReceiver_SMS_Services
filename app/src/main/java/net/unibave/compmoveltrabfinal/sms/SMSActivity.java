a80febfe4eb6a3e9d3e67227f7f40a3016e00cb9
rf17 <rf17@outlook.com.br>
2016-06-05 15:04:19 -0300

    Melhorado visual das telas.

--- app/src/main/java/net/unibave/compmoveltrabfinal/sms/SMSActivity.java
+++ app/src/main/java/net/unibave/compmoveltrabfinal/sms/SMSActivity.java
@@ -1,14 +1,9 @@
 package net.unibave.compmoveltrabfinal.sms;
 
-import android.content.pm.PackageManager;
-import android.os.Build;
-import android.support.annotation.NonNull;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.SmsManager;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import net.unibave.compmoveltrabfinal.R;
 
@@ -18,64 +13,20 @@ import net.unibave.compmoveltrabfinal.R;
  */
 public class SMSActivity extends AppCompatActivity {
 
-    private EditText editTextTelefone;
-    private EditText editTextMessage;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
-
-        editTextTelefone = (EditText) findViewById(R.id.editTextTelefone);
-        editTextMessage = (EditText) findViewById(R.id.editTextMessage);
     }
 
     public void btnSendSmsOnClick(View view) {
-        try {
-            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
-                String[] perms = {"android.permission.SEND_SMS"};
-                requestPermissions(perms, 200);
-            }else{
-                sendSMS();
-            }
-        } catch (Exception e) {
-            Toast.makeText(this, "Erro: "+e.getMessage(), Toast.LENGTH_LONG).show();
-        }
+        Intent intent = new Intent(this, SendSMSActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    public void onRequestPermissionsResult(int permsRequestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        try {
-            switch (permsRequestCode) {
-                case 200:
-                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                        sendSMS();
-                    } else {
-                        Toast.makeText(this, "Sem permissão do dispositivo para envio de SMS!", Toast.LENGTH_LONG).show();
-                    }
-                    break;
-                default:
-                    Toast.makeText(this, "Sem permissão do dispositivo para envio de SMS!", Toast.LENGTH_LONG).show();
-                    break;
-            }
-        } catch (Exception e) {
-            Toast.makeText(this, "Erro: "+e.getMessage(), Toast.LENGTH_LONG).show();
-        }
-
-    }
-
-    private void sendSMS(){
-        String phoneNo = editTextTelefone.getText().toString();
-        String message = editTextMessage.getText().toString();
-
-        try {
-            SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(phoneNo, null, message, null, null);
-            Toast.makeText(getApplicationContext(), "SMS enviado.", Toast.LENGTH_LONG).show();
-        } catch (Exception e) {
-            Toast.makeText(getApplicationContext(), "Erro no envio do SMS.", Toast.LENGTH_LONG).show();
-            e.printStackTrace();
-        }
+    public void btnReceiveSmsOnClick(View view) {
+        //Intent intent = new Intent(this, SendSMSActivity.class);
+        //startActivity(intent);
     }
 
 }
---
+++ app/src/main/java/net/unibave/compmoveltrabfinal/sms/SendSMSActivity.java
@@ -0,0 +1,77 @@
+package net.unibave.compmoveltrabfinal.sms;
+
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import net.unibave.compmoveltrabfinal.R;
+
+public class SendSMSActivity extends AppCompatActivity {
+
+    private EditText editTextTelefone;
+    private EditText editTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_sms);
+
+        editTextTelefone = (EditText) findViewById(R.id.editTextTelefone);
+        editTextMessage = (EditText) findViewById(R.id.editTextMessage);
+    }
+
+    public void btnSendSmsOnClick(View view) {
+        try {
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+                String[] perms = {"android.permission.SEND_SMS"};
+                requestPermissions(perms, 200);
+            }else{
+                sendSMS();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Erro: "+e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int permsRequestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        try {
+            switch (permsRequestCode) {
+                case 200:
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        sendSMS();
+                    } else {
+                        Toast.makeText(this, "Sem permissão do dispositivo para envio de SMS!", Toast.LENGTH_LONG).show();
+                    }
+                    break;
+                default:
+                    Toast.makeText(this, "Sem permissão do dispositivo para envio de SMS!", Toast.LENGTH_LONG).show();
+                    break;
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Erro: "+e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private void sendSMS(){
+        String phoneNo = editTextTelefone.getText().toString();
+        String message = editTextMessage.getText().toString();
+
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, message, null, null);
+            Toast.makeText(getApplicationContext(), "SMS enviado.", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Erro no envio do SMS.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+}
